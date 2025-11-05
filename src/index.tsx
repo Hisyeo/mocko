dbb7875c31b93fb07f18049c009e04a4193678da
MadcapJake <madcap.russo@gmail.com>
2025-11-04 22:55:07 -0600

    Add service worker registration for offline usage capability

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { AppProvider } from './AppContext';
 
 const root = ReactDOM.createRoot(
@@ -20,3 +21,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorkerRegistration.register();
